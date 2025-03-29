0b890a5caa0ff62179f6c3bdebe8526559fecccd
Manahil-Nadeem <manahilnadeem068@gmail.com>
2025-03-30 00:48:22 +0500

    Add files via upload

---
+++ Assignment03_Diamond-Pattern.py
@@ -0,0 +1,16 @@
+def draw_diamond(height):
+    #For upper part of the diamond (pyramid)
+    for i in range(height):
+        stars = '*' * (2 * i + 1)
+        spaces = ' ' * (height - i - 1)
+        print(spaces + stars)
+    
+    # For lower part of the diamond (inverted pyramid)
+    for i in range(height - 2, -1, -1):
+        stars = '*' * (2 * i + 1)
+        spaces = ' ' * (height - i - 1)
+        print(spaces + stars)
+
+# Calling the function with height 5
+draw_diamond(5)
+
